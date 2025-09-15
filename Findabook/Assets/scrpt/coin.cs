7c678b92d15ce2aabbf5d8e54264c82da4b2955e
H4xch1 <nugigaming982@gmail.com>
2025-09-15 10:57:03 +0700

    7. collect appear
    for mechanic plays

--- Findabook/Assets/scrpt/coin.cs
+++ Findabook/Assets/scrpt/coin.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    public GameObject nextBall; // Assign Ball2 in the Inspector
+    public GameObject[] balls; // Array to hold Ball1, Ball2, Ball3
+
+    void Start()
+    {
+        for (int i = 0; i < balls.Length; i++)
+        {
+            balls[i].SetActive(i == 0); // Only activate the first ball
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            if (nextBall != null)
+            {
+                nextBall.SetActive(true); // Show Ball2
+            }
             // Add score, play sound, etc.
             Destroy(gameObject); // Remove the collectible
         }
