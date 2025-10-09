d89573cb9f4f8639447c1458f6aece9f357429d2
Abil-tech <147292281+Abil-tech@users.noreply.github.com>
2025-10-09 10:03:48 +0700

    setting

---
+++ Findabook/Assets/scrpt/SettingS.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingS : MonoBehaviour
+{
+    public void ButtonBack()
+    {
+      SceneManager.LoadScene("MainMenu");
+    }
+    
+}
